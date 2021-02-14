381e8cbdbd9315ff189dbdcd3bdd503dbc08793a
sam@jetson29 <sxu7@uncc.edu>
2021-02-13 21:46:07 -0500

    both detection method done
    resnet and mobilenet_v2,
    ptflops done for resnet,
    still need ptflops for mobilenet_v2

--- hw0/mobilenetv2.py
+++ hw0/mobilenetv2.py
@@ -6,20 +6,16 @@ import argparse
 # parse the command line
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", type=str, help="filename of the image to process")
-parser.add_argument("--network", type=str, default="mobilenet_v2", help="model to use, can be:  googlenet, resnet-18, ect.")
+#parser.add_argument("--network", type=str, default="resnet-101", help="model to use, can be:  googlenet, resnet-18, ect.")
 args = parser.parse_args()
 
-# load an image (into shared CPU/GPU memory)
 img = jetson.utils.loadImage(args.filename)
 
-# load the recognition network
-net = jetson.inference.imageNet(args.network)
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 
-# classify the image
-class_idx, confidence = net.Classify(img)
+camera = jetson.utils.videoSource("/dev/video0")      # '/dev/video0' for V4L2
+display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
 
-# find the object description
-class_desc = net.GetClassDesc(class_idx)
-
-# print out the result
-print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(class_desc, class_idx, confidence * 100))
+detections = net.Detect(img)
+display.Render(img)
+display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
