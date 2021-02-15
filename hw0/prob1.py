5a807f569d0957295d8d0b78ca9a9ef78e4a7ca7
sam@jetson29 <sxu7@uncc.edu>
2021-02-15 17:34:28 -0500

    file org

---
+++ hw0/prob1.py
@@ -0,0 +1,40 @@
+from torchvision import models
+from torchvision import transforms
+import torch
+from PIL import Image
+
+# Select resnet
+resnet = models.resnet101(pretrained=True)
+
+#preprocess = transforms.Compose([transfroms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+preprocess = transforms.Compose([
+    transforms.Resize(256), 
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )])
+
+# Load Image
+img = Image.open("img_0.jpg")
+img_t = preprocess(img)
+
+batch_t = torch.unsqueeze(img_t, 0)
+
+# Put into infernece mode
+resnet.eval()
+
+out = resnet(batch_t)
+
+with open('imagenet_classes.txt') as f:
+   labels = [line.strip() for line in f.readlines()]
+
+_, index = torch.max(out, 1)
+
+percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+print(labels[index[0]], percentage[index[0]].item())
+
+_, indices = torch.sort(out, descending=True)
+for label in [(labels[idx], percentage[idx].item()) for idx in indices[0][:5]]:
+   print(label)
